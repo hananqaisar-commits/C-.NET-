6c2b594550cc30da4ca330bb795acbc199311eb5
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-07-27 18:58:27 +0500

    Calculator by using Delegates

---
+++ Advanced C#/Delegates/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Calculator;
+
+public class Program
+{
+    delegate double DelegateOperation(double x, double y);//It will store the refernce of all methods have two same parameters
+    public static void Main(string[] args)
+    {
+        DelegateOperation opr;
+        opr = Add;
+        Console.WriteLine(opr(221.3, 56.3));
+        opr = Subtract;
+        Console.WriteLine(opr(221.3, 56.3));
+
+        Console.WriteLine("--------Pass delgate as parameter --------");
+        // now pass delegate as parameter
+        Calaculate(43.5, 23.9, Multiply);
+        Calaculate(43.5, 23.9, Divide);
+    }
+    static void Calaculate(double a, double b, DelegateOperation op)
+    {
+        Console.WriteLine($"{op(a, b):F2}");//upto 2 decimal after digit
+    }
+    static double Add(double a, double b) => a + b;
+    static double Subtract(double a, double b) => a - b;
+    static double Multiply(double a, double b) => a * b;
+    static double Divide(double a, double b) => b != 0 ? a / b : double.NaN;
+}
