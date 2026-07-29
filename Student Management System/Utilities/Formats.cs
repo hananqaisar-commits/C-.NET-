a1abcc42a255cf1509d5d3a061a00fef6716f547
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-07-29 11:58:07 +0500

    Declared methods as static

--- Student Management System/Utilities/Formats.cs
+++ Student Management System/Utilities/Formats.cs
@@ -4,20 +4,20 @@ namespace Utilities.Formats;
 
 public class Formats
 {
-    public void Header(String name)// It will print the header for the given name with proper formatting.
+    public static void Header(String name)// It will print the header for the given name with proper formatting.
     {
         Console.WriteLine("\n================================ " + name + " ================================");
     }
-    public void HeaderLine()
+    public static void HeaderLine()
     {
         Console.WriteLine("\n-------------------------------------------------------------------------------");
     }
-    public void Loading(String loadingContext)
+    public static void Loading(String loadingContext)
     {
         Console.WriteLine($"{loadingContext}...");
     }
 
-    public void Exit()// It will print the exit message when the user chooses to exit the program.
+    public static void Exit()// It will print the exit message when the user chooses to exit the program.
     {
         Console.WriteLine("\nThank you for using");
         Console.WriteLine("Student Management System");
@@ -25,11 +25,11 @@ public class Formats
     }
 
 
-    public void HeaderTexts()
+    public static void HeaderTexts()
     {
         Console.WriteLine($"{"ID",-8} {"Name",-20} {"Marks",-6}");
     }
-    public void menu()// It will print the menu options for the user to choose from it.
+    public static void menu()// It will print the menu options for the user to choose from it.
     {
         Console.WriteLine("\n1. Add Student");
         Console.WriteLine("2. Add Teacher");
@@ -42,7 +42,7 @@ public class Formats
 
     }
 
-    public void continuePrompt()// It will prompt the user to press any key to continue after performing an action.
+    public static void continuePrompt()// It will prompt the user to press any key to continue after performing an action.
     {
         Console.WriteLine("\nPress any key to continue...");
         Console.ReadKey();// Wait for the user to press a key before continuing
