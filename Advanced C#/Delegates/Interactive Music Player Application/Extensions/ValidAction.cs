6d7c4c38b93f1d043d78caecee7d1761b8177a76
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-07-31 11:38:45 +0500

    Add Vlaideation of Music Player INput in the Extension Methods

--- Advanced C#/Delegates/Interactive Music Player Application/Extensions/ValidAction.cs
+++ Advanced C#/Delegates/Interactive Music Player Application/Extensions/ValidAction.cs
@@ -2,8 +2,12 @@ namespace Extensions.ValidAction;
 
 public static class ValidAction
 {
-    public static bool IsValidSongInput(this string songName)
-    {
-        return !string.IsNullOrWhiteSpace(songName);
-    }
+    public static bool IsValidSongInput(this string songName) =>
+         !string.IsNullOrWhiteSpace(songName);
+
+    public static bool IsValidMusicPlayerAction(this string musicPlayerAction) =>
+
+        string.IsNullOrWhiteSpace(musicPlayerAction);
+
+
 }
