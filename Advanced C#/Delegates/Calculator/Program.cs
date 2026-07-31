62f3f407637349fbfee96d3b2cca14a27f125344
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-07-31 14:54:58 +0500

    Added More Delegate Use

--- Advanced C#/Delegates/Calculator/Program.cs
+++ Advanced C#/Delegates/Calculator/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Reflection.Metadata.Ecma335;
-
 namespace Calculator;
 
 public class Program
 {
     delegate double DelegateOperation(double x, double y);//It will store the refernce of all methods have two same parameters
+    delegate void DelegatePrint(string name);
     public static void Main(string[] args)
     {
         DelegateOperation opr;
@@ -18,6 +18,17 @@ public class Program
         // now pass delegate as parameter
         Calaculate(43.5, 23.9, Multiply);
         Calaculate(43.5, 23.9, Divide);
+
+        DelegatePrint print;
+
+        print = Name;
+        print += WhatsAppNotify;
+        print += SmsNotify;
+        print += EmailAppNotify;
+
+        Console.WriteLine("--------Delegate invoke all operations in 1 call --------");
+
+        print("Hanan");
     }
     static void Calaculate(double a, double b, DelegateOperation op)
     {
@@ -27,4 +38,9 @@ public class Program
     static double Subtract(double a, double b) => a - b;
     static double Multiply(double a, double b) => a * b;
     static double Divide(double a, double b) => b != 0 ? a / b : double.NaN;
+
+    static void Name(string name) => Console.WriteLine("Hanan Qaisar\n");
+    static void SmsNotify(string name) => Console.WriteLine($"Sms Notification sent to {name}\n");
+    static void WhatsAppNotify(string name) => Console.WriteLine($"Whatsapp Notification sent to {name}\nConfirm your WhatsApp OTP\n");
+    static void EmailAppNotify(string name) => Console.WriteLine($"Email Notification sent to {name}\nWant to change Password?\n\tClick this url\n");
 }
