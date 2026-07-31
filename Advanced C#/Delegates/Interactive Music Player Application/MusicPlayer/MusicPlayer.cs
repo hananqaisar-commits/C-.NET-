0188a0ff5a64a1f67f37f3abaa5d5c050c21a260
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-07-31 11:12:48 +0500

    Remove Delgate & use built in Action delegate

--- Advanced C#/Delegates/Interactive Music Player Application/MusicPlayer/MusicPlayer.cs
+++ Advanced C#/Delegates/Interactive Music Player Application/MusicPlayer/MusicPlayer.cs
@@ -3,32 +3,18 @@ using System.Runtime.CompilerServices;
 
 namespace Player.MusicPlayer;
 
-public delegate void EventHandler(string songName);//custom delegate
 public class MusicPlayer
 {
-    public event EventHandler playEvent;
-    public event EventHandler pauseEvent;
-    public event EventHandler stopEvent;
-    public event EventHandler skipEvent;
-    public void Play(string songName)
-    {
-        Console.WriteLine($"Song Played {songName}");
-        playEvent?.Invoke(songName);
-    }
-    public void Pause(string songName)
-    {
-        Console.WriteLine($"Song Paused {songName}");
-        pauseEvent?.Invoke(songName);
-    }
-    public void Stop(string songName)
-    {
-        Console.WriteLine($"Song Stoped {songName}");
-        stopEvent?.Invoke(songName);
-    }
+    public event Action<string> playEvent;
+    public event Action<string> pauseEvent;
+    public event Action<string> stopEvent;
+    public event Action<string> skipEvent;
+    public void Play(string songName) => playEvent?.Invoke(songName);
 
-    public void Skipped(string songName)
-    {
-        Console.WriteLine($"Song Skipped {songName}");
-        skipEvent?.Invoke(songName);
-    }
+    public void Pause(string songName) => pauseEvent?.Invoke(songName);
+
+    public void Stop(string songName) => stopEvent?.Invoke(songName);
+
+
+    public void Skipped(string songName) => skipEvent?.Invoke(songName);
 }
