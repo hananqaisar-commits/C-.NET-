bd857304733d608a9751ccd1d634adbd8cd34fdb
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-08-03 13:08:47 +0500

    Perform sorting by different methods

--- Advanced C#/LINQ/Queries/Sort.cs
+++ Advanced C#/LINQ/Queries/Sort.cs
@@ -1,29 +1,26 @@
 using System;
 using System.Linq;
+using Interface.IHasPersonInfo;
 using Models.Person;
+
 namespace Queries.Sort;
 
 public class Sort
 {
-    public List<Person> ByAgeAccending(List<Person> list)
-    {
-        var result = list.OrderBy(s => s.Age).ToList();
-        return result;
-    }
-    public List<Person> ByAgeDecending(List<Person> list)
-    {
-        var result = list.OrderByDescending(s => s.Age).ToList();
-        return result;
-    }
-    public List<Person> BySalaryAccending(List<Person> list)
-    {
-        var result = list.OrderBy(s => s.Salary).ToList();
-        return result;
-    }
-    public List<Person> BySalaryDecending(List<Person> list)
-    {
-        var result = list.OrderByDescending(s => s.Salary).ToList();
-        return result;
-    }
+    public List<T> ByAgeAccending<T>(List<T> list) where T : IHasPersonInfo =>
+         list.OrderBy(s => s.Age).ToList<T>();
 
+
+    public List<K> ByAgeDecending<K>(List<K> list) where K : IHasPersonInfo
+
+        => list.OrderByDescending(s => s.Age)
+        .ToList();
+    public List<T> ByNameAccending<T>(List<T> list) where T : IHasPersonInfo =>
+         list.OrderBy(s => s.Name).ToList<T>();
+
+
+    public List<K> ByNameDecending<K>(List<K> list) where K : IHasPersonInfo
+
+        => list.OrderByDescending(s => s.Name)
+        .ToList();
 }
