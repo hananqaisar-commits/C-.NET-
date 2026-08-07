ad2b3ce1ee77208655769d542e3db28ca138dbc1
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-08-07 15:47:20 +0500

    feat: add character frequency counter using Dictionary

---
+++ Collections/Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿count();
+static void count()
+{
+    Dictionary<char, int> dict = new Dictionary<char, int>();
+    using StreamReader reader = new StreamReader("/home/hanan/Desktop/C#(.NET)/Collections/Dictionary/text.txt");
+
+    string? line;
+
+    while ((line = reader.ReadLine()) != null)
+    {
+        foreach (char ch in line)
+        {
+            dict[ch] = dict.GetValueOrDefault(ch) + 1;
+        }
+
+    }
+
+    foreach (var item in dict)
+    {
+        Console.WriteLine($"{item.Key} : {item.Value}");
+    }
+}
