c1400a7e60a0f54efa5856a6a25dfa6503657fa2
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-08-10 11:11:43 +0500

    Now add method to count words also

--- Collections/Dictionary/Program.cs
+++ Collections/Dictionary/Program.cs
@@ -1,9 +1,9 @@
-﻿count();
-static void count()
+﻿countCharacters();
+countWords();
+static void countCharacters()
 {
     Dictionary<char, int> dict = new Dictionary<char, int>();
     using StreamReader reader = new StreamReader("/home/hanan/Desktop/C#(.NET)/Collections/Dictionary/text.txt");
-
     string? line;
 
     while ((line = reader.ReadLine()) != null)
@@ -11,6 +11,24 @@ static void count()
         foreach (char ch in line)
         {
             dict[ch] = dict.GetValueOrDefault(ch) + 1;
+        }
+    }
+    foreach (var item in dict)
+    {
+        Console.WriteLine($"{item.Key} : {item.Value}");
+    }
+}
+
+static void countWords()
+{
+    Dictionary<string, int> dict = new Dictionary<string, int>();
+    using StreamReader reader = new StreamReader("/home/hanan/Desktop/C#(.NET)/Collections/Dictionary/text.txt");
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        foreach (string word in line.Split())
+        {
+            dict[word] = dict.GetValueOrDefault(word) + 1;
         }
     }
     foreach (var item in dict)
